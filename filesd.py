24658c950de5475d8586646f090678b8cf821ac1
rey163025 <125667063+rey163025@users.noreply.github.com>
2023-07-06 14:50:59 +0300

    Update filesd.py

--- filesd.py
+++ filesd.py
@@ -1,12 +1,21 @@
 import datetime
 import os
+import platform
+
 
 def file_info():
     desktop = os.path.join(os.path.expanduser("~"), "Desktop")
 
-    for file in os.listdir(desktop):
-        print(f"Name: {file} # Path: {os.path.join(desktop, file)} # Size: {os.path.getsize(os.path.join(desktop, file))} bytes \
-# Creation date: {datetime.date.fromtimestamp(os.path.getctime(os.path.join(desktop,file)))}")
+    if platform.system() == "Darwin":
+        for file in os.listdir(desktop):
+            print(
+                f"Name: {file} # Path: {os.path.join(desktop, file)} # Size: {os.path.getsize(os.path.join(desktop, file))} bytes # Creation date: {datetime.date.fromtimestamp(os.stat(os.path.join(desktop,file)).st_birthtime)}"
+            )
+    elif platform.system() == "Windows":
+        for file in os.listdir(desktop):
+            print(
+                f"Name: {file} # Path: {os.path.join(desktop, file)} # Size: {os.path.getsize(os.path.join(desktop, file))} bytes # Creation date: {datetime.date.fromtimestamp(os.path.getctime(os.path.join(desktop,file)))}"
+            )
 
 
 file_info()
